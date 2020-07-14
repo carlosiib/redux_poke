7003ced83bbb761b71ed00112b2d2daf4077e92c
carlosiib <ntic.carlos@gmail.com>
2020-07-14 15:09:20 -0700

    Creation of Login and Navbar routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+
 import Pokemones from './componentes/Pokemones';
+import Navbar from "./componentes/Navbar"
+import Login from "./componentes/Login"
 
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="container mt-3">
-      <Pokemones />
-    </div>
+    <Router>
+      <div className="container mt-3">
+        <Navbar />
+
+        <Switch>
+          <Route component={Pokemones} path="/" exact />
+          <Route component={Login} path="/login" exact />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
